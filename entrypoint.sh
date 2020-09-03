41eaeac20b9e96cc69f4fae35abaa76baecc5949
Christian Schweinhardt <are.u.kidding@me.com>
2020-09-03 18:06:55 +0200

    refactor: modify entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,15 +11,21 @@ echo -e "Requirements path set as: " $REQS
 if [[ "$ENV_TYPE" == "venv" ]]; then
     pip install safety
     pip install -r $REQS
-    cd $MANAGE_PATH && safety check -o output.txt && safety check --json -o /output.json
+    cd $MANAGE_PATH && safety check --short-report &> output.txt
+    EXIT_CODE=$?
 fi
 if [[ -z "$ENV_TYPE" ]]; then
     echo "No virtual environment specified."
     pip install django
-    cd $MANAGE_PATH && safety check -o output.txt && safety check --json -o /output.json
+    cd $MANAGE_PATH && safety check --short-report &> output.txt
+    EXIT_CODE=$?
 fi
 
 echo -e "\n--------- Python Safety Check results ---------"
 cat output.txt
 
-python /check.py
+if [ "$EXIT_CODE" -eq 0 ]; then
+    exit 0
+else
+    exit 1
+fi
