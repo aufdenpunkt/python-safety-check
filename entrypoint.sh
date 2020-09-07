5a6dcb87559460ec46e0167b45a452f146d526a5
Christian Schweinhardt <are.u.kidding@me.com>
2020-09-07 13:33:10 +0200

    style: remove unused chars

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,37 +11,37 @@ SAFETY_ARGS=${INPUT_SAFETY_ARGS}
 SCAN_REQUIREMENTS_FILE_ONLY=${INPUT_SCAN_REQUIREMENTS_FILE_ONLY}
 
 echo -e "\n----------------- Variables -------------------"
-echo -e "${COLOR_YELLOW}INFO${COLOR_OFF}: Requirements path ${REQS}"
-echo -e "${COLOR_YELLOW}INFO${COLOR_OFF}: Scan requirements file only ${SCAN_REQUIREMENTS_FILE_ONLY}"
-echo -e "${COLOR_YELLOW}INFO${COLOR_OFF}: Safety args ${SAFETY_ARGS:-Not provided}"
+echo -e "${COLOR_YELLOW}INFO${COLOR_OFF} Requirements path: ${REQS}"
+echo -e "${COLOR_YELLOW}INFO${COLOR_OFF} Scan requirements file only: ${SCAN_REQUIREMENTS_FILE_ONLY}"
+echo -e "${COLOR_YELLOW}INFO${COLOR_OFF} Safety args: ${SAFETY_ARGS:-Not provided}"
 
 echo -e "\n------------ Install requirements -------------"
-echo -e "${COLOR_YELLOW}INFO${COLOR_OFF}: Call command $ pip install safety"
+echo -e "${COLOR_YELLOW}INFO${COLOR_OFF} Call command: pip install safety"
 pip install safety
 
 if [ "$SCAN_REQUIREMENTS_FILE_ONLY" == "false" ]
 then
-    echo -e "${COLOR_YELLOW}INFO${COLOR_OFF}: Call command $ pip install -r ${REQS}"
+    echo -e "${COLOR_YELLOW}INFO${COLOR_OFF} Call command: pip install -r ${REQS}"
     pip install -r $REQS
 else
-    echo -e "${COLOR_YELLOW}INFO${COLOR_OFF}: Skip installing python packages"
+    echo -e "${COLOR_YELLOW}INFO${COLOR_OFF} Skip installing python packages"
     SAFETY_ARGS="${SAFETY_ARGS} -r ${REQS}"
 fi
 
 echo -e "\n-------------- Do safety checks ---------------"
-echo -e "${COLOR_YELLOW}INFO${COLOR_OFF}: Call command $ safety check --short-report ${SAFETY_ARGS} &> output.txt"
+echo -e "${COLOR_YELLOW}INFO${COLOR_OFF} Call command: safety check --short-report ${SAFETY_ARGS} &> output.txt"
 safety check --short-report $SAFETY_ARGS &> output.txt
 EXIT_CODE=$?
 
 echo -e "\n--------- Python Safety Check results ---------"
-echo -e "${COLOR_YELLOW}INFO${COLOR_OFF}: Call command $ cat output.txt"
+echo -e "${COLOR_YELLOW}INFO${COLOR_OFF} Call command: cat output.txt"
 cat output.txt
 
 if [ "$EXIT_CODE" -eq 0 ]
 then
-    echo -e "\n${COLOR_GREEN}SUCCESS${COLOR_OFF}: exit 0"
+    echo -e "\n${COLOR_GREEN}SUCCESS${COLOR_OFF} exit 0"
     exit 0
 else
-    echo -e "\n${COLOR_RED}ERROR${COLOR_OFF}: exit 1"
+    echo -e "\n${COLOR_RED}ERROR${COLOR_OFF} exit ${EXIT_CODE}"
     exit 1
 fi
