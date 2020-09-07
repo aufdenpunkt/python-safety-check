3f240c8c63c6af8156af37a1d2674997999595ef
Christian Schweinhardt <are.u.kidding@me.com>
2020-09-07 12:53:07 +0200

    refactor: make entrypoint script more clear

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,37 +3,38 @@
 
 REQS=${GITHUB_WORKSPACE}/${DEP_PATH}
 SAFETY_ARGS=${INPUT_SAFETY_ARGS}
-USE_REQUIREMENTS_FILE_ONLY=${INPUT_USE_REQUIREMENTS_FILE_ONLY}
+SCAN_REQUIREMENTS_FILE_ONLY=${INPUT_SCAN_REQUIREMENTS_FILE_ONLY}
 
 echo -e "\n----------------- Variables -------------------"
-echo -e "Requirements path: " $REQS
-echo -e "Use requirements only: " $USE_REQUIREMENTS_FILE_ONLY
-echo -e "Safety args: ${SAFETY_ARGS:-Not provided}"
+echo -e "INFO: Requirements path " $REQS
+echo -e "INFO: Scan requirements file only " $SCAN_REQUIREMENTS_FILE_ONLY
+echo -e "INFO: Safety args ${SAFETY_ARGS:-Not provided}"
 
 echo -e "\n------------ Install requirements -------------"
-echo -e "Call command: pip install safety"
+echo -e "INFO: Call command $ pip install safety"
 pip install safety
 
-if [ "$USE_REQUIREMENTS_FILE_ONLY" -eq "false" ]; then
-    echo -e "Call command: pip install -r ${REQS}"
+if [ "$SCAN_REQUIREMENTS_FILE_ONLY" -eq "false" ]; then
+    echo -e "INFO: Call command $ pip install -r ${REQS}"
     pip install -r $REQS
 else
+    echo -e "INFO: Skip installing python packages"
     SAFETY_ARGS="${SAFETY_ARGS} -r ${REQS}"
 fi
 
 echo -e "\n-------------- Do safety checks ---------------"
-echo -e "Call command: safety check --short-report ${SAFETY_ARGS} &> output.txt"
+echo -e "INFO: Call command $ safety check --short-report ${SAFETY_ARGS} &> output.txt"
 safety check --short-report $SAFETY_ARGS &> output.txt
 EXIT_CODE=$?
 
 echo -e "\n--------- Python Safety Check results ---------"
-echo -e "Call command: cat output.txt"
+echo -e "INFO: Call command $ cat output.txt"
 cat output.txt
 
 if [ "$EXIT_CODE" -eq 0 ]; then
-    echo -e "\nexit 0"
+    echo -e "\nINFO: exit 0"
     exit 0
 else
-    echo -e "\nexit 1"
+    echo -e "\nINFO: exit 1"
     exit 1
 fi
