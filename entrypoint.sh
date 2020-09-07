2c1ec2430a79a9d156e7cec52475eff4aefc9024
Christian Schweinhardt <are.u.kidding@me.com>
2020-09-07 12:42:52 +0200

    feat: add possibility to do the vulnerabilities scan against requirements file only

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,23 @@
 
 REQS=${GITHUB_WORKSPACE}/${DEP_PATH}
 SAFETY_ARGS=${INPUT_SAFETY_ARGS}
-
+USE_REQUIREMENTS_FILE_ONLY=${INPUT_USE_REQUIREMENTS_FILE_ONLY}
 
 echo -e "\n----------------- Variables -------------------"
 echo -e "Requirements path: " $REQS
+echo -e "Use requirements only: " $USE_REQUIREMENTS_FILE_ONLY
 echo -e "Safety args: ${SAFETY_ARGS:-Not provided}"
 
-echo -e "\n---------- Install pip requirements -----------"
+echo -e "\n------------ Install requirements -------------"
 echo -e "Call command: pip install safety"
 pip install safety
-echo -e "Call command: pip install -r ${REQS}"
-pip install -r $REQS
+
+if [ "$USE_REQUIREMENTS_FILE_ONLY" -eq "false" ]; then
+    echo -e "Call command: pip install -r ${REQS}"
+    pip install -r $REQS
+else
+    SAFETY_ARGS="${SAFETY_ARGS} -r ${REQS}"
+fi
 
 echo -e "\n-------------- Do safety checks ---------------"
 echo -e "Call command: safety check --short-report ${SAFETY_ARGS} &> output.txt"
@@ -21,11 +27,13 @@ safety check --short-report $SAFETY_ARGS &> output.txt
 EXIT_CODE=$?
 
 echo -e "\n--------- Python Safety Check results ---------"
-echo -e "cat output.txt"
+echo -e "Call command: cat output.txt"
 cat output.txt
 
 if [ "$EXIT_CODE" -eq 0 ]; then
+    echo -e "\nexit 0"
     exit 0
 else
+    echo -e "\nexit 1"
     exit 1
 fi
